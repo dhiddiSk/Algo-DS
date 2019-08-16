af35752d256a63f3881d4049aaeb231313fad1c4
saikrishna <saikrishna.4369@gmail.com>
2019-08-16 21:55:06 +0200

    still not yet completed

--- src/main/java/Quick_sort.java
+++ src/main/java/Quick_sort.java
@@ -4,62 +4,57 @@ element and comparing remaining elements left and right to it. Thus, results in 
 side of pivot and greater to the right side to it.
 Then after the elements in the left and right of pivot are sorted using the same procedure also sorted. whcih finally
 results in an completely sorted array.
+ step1 : Derive an pivot element and then compare all the elements of the array with the pivot element, such that the left
+         ones are smaller and the right ones are greater than the pivot element
+ step2:  Similarly carry the same operation on the left array and the right array i.e define a pivot element and then bacck
+         to step one.
+
+  do this iteratively until the whole array is sorted completely
  */
+import java.util.Arrays;
+public class Quick_sort{
+    private static void partition(int left, int right,int[] arrayInput) {
+       int pivot = arrayInput[right];
+       int lastButOne = right-1;
+       /*
+             left elements of the array such that the left array elements are smaller than pivot.
+             */
+                 while(left < lastButOne){
+                    while(arrayInput[left]<pivot && arrayInput[lastButOne]>pivot){
+                        ++left;
+                        --lastButOne;
+                          }
+                    while(arrayInput[left]>=pivot && arrayInput[lastButOne]>pivot){
+                        --lastButOne;
+                         }
+                   while(arrayInput[left]<pivot && arrayInput[lastButOne]<=pivot){
+                       ++left;
+                   }
+                    if(arrayInput[left] > pivot && arrayInput[lastButOne] <= pivot){
+                         Quick_sort.swap(arrayInput,left,lastButOne);
+                         ++left;
+                         --lastButOne;
+                        }
+                    else{
+                        ++left;
+                        --lastButOne;
+                    }
+                   }
+        System.out.println(Arrays.toString(arrayInput));
+               }
 
-
-class Quick_sort_core{
-    public int[] LA = new int[6];
-    public int PE, temp1,temp2;
-    public int[] RA = new int[6];
-    Quick_sort_core(int[] LA ,int[] RA,int PE){
-        this.LA = LA;
-        this.RA = RA; this.PE = PE;
+     public static void swap(int[] arrayInput, int left, int lastButOne){
+        /*
+        Swapping the elements
+         */
+          int temp = arrayInput[left];
+          arrayInput[left] = arrayInput[lastButOne];
+          arrayInput[lastButOne] = temp;
     }
-    public void comparator(){
-        for(int i =0 ; i<=LA.length;i++){                        // LA -left Array, RA- Right Array, PE- Pivot Element
-            if(PE > LA[i]){
-                System.out.println(LA);
-            }
-            else{
-                temp1 = PE;
-                PE = LA[i];
-                LA[i] = temp1;
-            }
-        }
-        for(int j = LA.length+1; j<=RA.length;j++){
-            if(PE < RA[j]){
-                System.out.println(RA);
-            }
-            else{
-                temp2 = PE;
-                PE = RA[j];
-                RA[j] = temp2;
-            }
-        }
 
-    }
-}
-public class Quick_sort {
     public static void main(String[] args){
-        int[] array_input = new int[]{6,7,8,9,10,1,2,3,4,5};
-        int pivot_element,position;
-        if((array_input.length%2)==0){
-            position = ((array_input.length)/2);
-            pivot_element = array_input[position];
-        }
-        else{
-            position = (((int)((array_input.length)/2)+1));
-            pivot_element = array_input[position];
-        }
-        //System.out.println(pivot);
-        int[] array_left = new int[position];
-        int[] array_right = new int[array_input.length - position];
-        if((array_right.length - array_left.length)>0){
-            Quick_sort_core  object1 = new Quick_sort_core(array_left , array_right, pivot_element);
-
-        }
-
-
+        int[] arrayInput = new int[]{3, 4, 1, 32, 0, 1, 5, 12 ,2, 5 ,7 ,8 ,9, 2, 44, 111, 5};
+          int left =0, right = (arrayInput.length)-1;
+        Quick_sort.partition(left,right,arrayInput);
     }
 }
-
